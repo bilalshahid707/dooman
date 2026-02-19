340a137a6c71cebd76fdcb7a5713d5e526c8adb5
mirza fateh <fateh.fc7@gmail.com>
2026-02-19 15:52:22 +0500

    depl fixes

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://dooman-git-main-bilalshahid707s-projects.vercel.app/"],
+    allow_origins=["https://dooman-git-main-bilalshahid707s-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
